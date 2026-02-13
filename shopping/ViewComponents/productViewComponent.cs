60d2e25c9397e9f2ea28d418705c9f1599c17501
EA <easgarii1385@gmail.com>
2026-02-13 22:59:02 +0330

    dynamic product

---
+++ shopping/ViewComponents/productViewComponent.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using shopping.Models;
+
+namespace shopping.ViewComponents
+{
+    public class productViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var product = new List<product>
+            {
+                new product(1, "پسته ممتاز", "سرشار از پروتئین", "1.jpg"),
+                new product(2, "بادام درختی", "مناسب سلامت قلب", "2.jpg"),
+                new product(3, "گردو تازه", "تقویت حافظه", "3.jpg"),
+                new product(4, "کشمش طلایی", "منبع طبیعی آهن", "4.jpg"),
+                new product(5, "زعفران اصل", "کیفیت صادراتی", "5.jpg"),
+                new product(6, "دارچین سیلان", "عطر فوق‌العاده", "6.jpg"),
+                new product(7, "زنجبیل تازه", "تقویت ایمنی بدن", "7.jpg"),
+                new product(8, "برنج طارم", "معطر و ممتاز", "8.jpg"),
+                new product(9, "برنج هاشمی", "کیفیت اعلا", "10.jpg"),
+            };
+         return View("_product",product);
+        }
+    }
+}
---
+++ shopping/wwwroot/asset/js/site.js
@@ -0,0 +1,23 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+function filterProducts(category, btn) {
+    let cards = document.querySelectorAll('.card');
+    let buttons = document.querySelectorAll('.filter button');
+
+    // حذف کلاس فعال از همه دکمه‌ها
+    buttons.forEach(b => b.classList.remove('active'));
+    // اضافه کردن کلاس فعال به دکمه فعلی
+    btn.classList.add('active');
+
+    // فیلتر کردن کارت‌ها بر اساس دسته‌بندی
+    cards.forEach(card => {
+        if (category === 'all') {
+            card.style.display = "block";
+        } else {
+            card.style.display = card.classList.contains(category) ? "block" : "none";
+        }
+    });
+}
+
