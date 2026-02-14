d9dbb9b38e83fcf2cec935c501f61549a8a74b56
EA <easgarii1385@gmail.com>
2026-02-14 09:59:07 +0330

    new option of dfghjkasdfghjkll

--- shopping/ViewComponents/productViewComponent.cs
+++ shopping/ViewComponents/productViewComponent.cs
@@ -20,6 +20,8 @@ namespace shopping.ViewComponents
                 new product(8, "برنج طارم", "معطر و ممتاز", "8.jpg"),
                 new product(9, "برنج هاشمی", "کیفیت اعلا", "10.jpg"),
                 new product(9, "برنج هاشمی", "کیفیت اعلا", "10.jpg"),
+                new product(9, "برنج هاشمی", "کیفیت اعلا", "10.jpg"),
+
             };
          return View("_product",product);
         }
